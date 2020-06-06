3c226d2a09d21a5db11983e206f148b1d9e3831f
Jonathan Sarmiento <john.sarmiento@me.com>
2020-06-06 14:55:02 -0500

    Final Test

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 
 # Title: undefined
-Description: 
-Table of Contents: 
-Installation: 
-Usage: 
-Contributing: 
-Tests: 
-Questions: 
+Description: Node.js app that generates Readme files
+Table of Contents: ReadME.txt
+Installation: None Required
+Usage: Homework Assigment
+Contributing: John.S
+Tests: ReadMe HW
+Questions: Can You Create a Read Me Gen App?
 [![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/)
-    
--- index.js
+++ index.js
@@ -60,7 +60,7 @@ inquirer.prompt([
         badgeUrl = '[![License](https://img.shields.io/badge/License-EPL%201.0-red.svg)](https://opensource.org/licenses/EPL-1.0)'
     } else if (answer.License === "IBM") {
         badgeUrl = '[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)'
-    } else if (answer.License === "Mozilla") {
+    } else (answer.License === "Mozilla") {
         badgeUrl = '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)'
     }
 
